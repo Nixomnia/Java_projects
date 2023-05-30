edba6806e90adf427841a20e5d43ff64e26f89d8
Nixomnia <119518897+Nixomnia@users.noreply.github.com>
2023-05-30 09:54:40 +0530

    Add files via upload

---
+++ MyDataTypes.java
@@ -0,0 +1,32 @@
+
+class MyDataTypes {
+    public static void main(String args[]){
+
+        String name = "Stanley";
+        System.out.println(name);
+        byte b = 25 ;
+        System.out.println(b);
+        short s = -32456;
+        System.out.println(s);
+        int i = 23456789;
+        System.out.println(i);
+        long l = 1000000000;
+        System.out.println(l);
+        float f = 9.1e-3F;
+        System.out.println(f);
+        double d = 56.90455678;
+        System.out.println(d);
+        char c = 'c';
+        System.out.println(c);
+        boolean h = true;
+        System.out.println(h);
+
+
+
+
+
+
+
+    }
+
+}
