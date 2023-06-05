850fb00dfb82948905a05ec152deb5e629e58f2a
Nixomnia <119518897+Nixomnia@users.noreply.github.com>
2023-06-05 14:23:25 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class Task4{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 1st number : ");
+        double num1 = sc.nextDouble();
+        System.out.println("Enter 2nd number : ");
+        double num2 = sc.nextDouble();
+        System.out.println("Choose operation +,-,* or / :");
+        char operation = sc.next().charAt(0);
+        if(operation == '+'){
+            System.out.println(String.format("Ans = %f",num1+num2));
+        }
+        else if(operation == '-'){
+            System.out.println(String.format("Ans = %f",num1 - num2));
+        }
+        else if(operation == '*'){
+            System.out.println(String.format("Ans = %f",num1 * num2));
+        }
+        else if(operation == '/'){
+            System.out.println(String.format("Ans = %f",num1 / num2));
+        }
+
+
+
+    }
+
+}
