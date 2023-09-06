b049b715dca0e3b4620775e8935670219eb89f55
Nixomnia <119518897+Nixomnia@users.noreply.github.com>
2023-09-06 13:36:45 +0530

    Add files via upload

---
+++ DuplicateCheck_Sets.java
@@ -0,0 +1,24 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+class Sets{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Set<Double> numberSet = new HashSet<>();
+        int count = 0;
+        boolean loop = true;
+        while(true){
+            System.out.print("Enter a number: ");
+            double temp  =sc.nextDouble() ;
+            if(numberSet.contains(temp)){
+                System.out.println("Duplicate number.");
+            }
+            else {
+                System.out.println("Unique, adding to set...");
+                numberSet.add(temp);
+            }
+        }
+    }
+}
+
